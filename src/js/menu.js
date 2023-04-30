f1493fc6f3f28ffa3c4ffd09f5655f0df428a367
Anastasia <73635691+ganyushinaanastasia@users.noreply.github.com>
2023-04-30 16:59:22 +0300

    fix js in results

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,26 +1,18 @@
 function dropdown() {
 
-    let sign1 = document.getElementById("sign1");
-    let sign2 = document.getElementById("sign2");
-    let sign3 = document.getElementById("sign3");
+    const dropdowns = document.querySelectorAll('.results__dropdowns__item');
 
-    let menu1 = document.getElementById("list1");
-    let menu2 = document.getElementById("list2");
-    let menu3 = document.getElementById("list3");
+    dropdowns.forEach(dropdown => {
+        const menu = dropdown.querySelector('.results__dropdowns__item__list');
+        const title = dropdown.querySelector('.results__dropdowns__item__head--title');
+        const sign = dropdown.querySelector('.results__dropdowns__item__head--sign');
+        const head = dropdown.querySelector('.results__dropdowns__item__head');
 
-    sign1.addEventListener("click", function() {
-    menu1.classList.toggle("dropdown");
-    title1.classList.toggle("color");
-    });
-
-    sign2.addEventListener("click", function() {
-    menu2.classList.toggle("dropdown");
-    title2.classList.toggle("color");
-    });
-
-    sign3.addEventListener("click", function() {
-    menu3.classList.toggle("dropdown");
-    title3.classList.toggle("color");
+        head.addEventListener("click", () => {
+            menu.classList.toggle("dropdown");
+            title.classList.toggle("color-title");
+            sign.classList.toggle("sign-change");
+        });
     });
 }
 
