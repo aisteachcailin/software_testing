6fcb24d540659a25c61a831b8cbb8facac3efe69
Anastasia <73635691+ganyushinaanastasia@users.noreply.github.com>
2023-05-02 19:33:32 +0300

    v2

--- src/js/menu.js
+++ src/js/menu.js
@@ -6,9 +6,8 @@ function dropdown() {
         const menu = dropdown.querySelector('.results__dropdowns__item__list');
         const title = dropdown.querySelector('.results__dropdowns__item__head--title');
         const sign = dropdown.querySelector('.results__dropdowns__item__head--sign');
-        const head = dropdown.querySelector('.results__dropdowns__item__head');
 
-        head.addEventListener("click", () => {
+        dropdown.addEventListener("click", () => {
             menu.classList.toggle("dropdown");
             title.classList.toggle("color-title");
             sign.classList.toggle("sign-change");
