0d419be912d2eb54d94b00a035a75e97676a7582
Anastasia <73635691+ganyushinaanastasia@users.noreply.github.com>
2023-05-12 16:41:47 +0300

    smooth accordion

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,19 +1,25 @@
+import $ from "jquery";
+
 function dropdown() {
 
-    const dropdowns = document.querySelectorAll('.results__dropdowns__item');
+    $('#list2, #list3').hide();
 
-    dropdowns.forEach(dropdown => {
-        const menu = dropdown.querySelector('.results__dropdowns__item__list');
-        const title = dropdown.querySelector('.results__dropdowns__item__head--title');
-        const sign = dropdown.querySelector('.results__dropdowns__item__head--sign');
+    $(".accordion1").click(function menu1() {
+        $('#list1').slideToggle();
+        $('#title1').toggleClass('color-title');
+        $('#sign1').toggleClass('sign-change');
+      });
+      $(".accordion2").click(function menu2() {
+        $('#list2').slideToggle();
+        $('#title2').toggleClass('color-title');
+        $('#sign3').toggleClass('sign-change');
+      });
+      $(".accordion3").click(function menu3() {
+        $('#list3').slideToggle();
+        $('#title3').toggleClass('color-title');
+        $('#sign3').toggleClass('sign-change');
+      });
 
-        dropdown.addEventListener("click", () => {
-            menu.classList.toggle("dropdown");
-            title.classList.toggle("color-title");
-            sign.classList.toggle("sign-change");
-        });
-    });
 }
-
 
 export default dropdown;
