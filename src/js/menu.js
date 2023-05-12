6d0327bceef8c452af7094275afc8b9f7332c3f9
Anastasia <73635691+ganyushinaanastasia@users.noreply.github.com>
2023-05-12 17:54:47 +0300

    slow slides

--- src/js/menu.js
+++ src/js/menu.js
@@ -5,17 +5,17 @@ function dropdown() {
     $('#list2, #list3').hide();
 
     $(".accordion1").click(function menu1() {
-        $('#list1').slideToggle();
+        $('#list1').slideToggle(800);
         $('#title1').toggleClass('color-title');
         $('#sign1').toggleClass('sign-change');
       });
       $(".accordion2").click(function menu2() {
-        $('#list2').slideToggle();
+        $('#list2').slideToggle(800);
         $('#title2').toggleClass('color-title');
         $('#sign3').toggleClass('sign-change');
       });
       $(".accordion3").click(function menu3() {
-        $('#list3').slideToggle();
+        $('#list3').slideToggle(800);
         $('#title3').toggleClass('color-title');
         $('#sign3').toggleClass('sign-change');
       });
